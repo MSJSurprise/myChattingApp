a48263cd4bcddd7aa643f1aea3fdec8330dc0510
Surprise <mesanwoolasurprise303>
2022-01-22 09:28:10 -0800

    Updates

--- app/src/main/java/com/example/mychattingapp/SignUp.kt
+++ app/src/main/java/com/example/mychattingapp/SignUp.kt
@@ -2,10 +2,43 @@ package com.example.mychattingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+//            val name = edtName.text.toString()
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
+        }
+
     }
+
+    private fun signUp(email: String, password: String) {
+        // logic for creating users
+
+    }
+
+
 }
