03014f99c161a5b680422f13c5df8eff017095c5
Surprise <mesanwoolasurprise303>
2022-01-22 08:17:49 -0800

    Created activity_login.xml

---
+++ app/src/main/java/com/example/mychattingapp/Login.kt
@@ -0,0 +1,20 @@
+package com.example.mychattingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class Login : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+    }
+}
