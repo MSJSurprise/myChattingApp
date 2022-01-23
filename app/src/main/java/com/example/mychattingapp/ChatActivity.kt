cabc2e7b19e483831f1111c5ef1d9dd12d621324
Surprise <mesanwoolasurprise303>
2022-01-23 02:22:02 -0800

    Updates

--- app/src/main/java/com/example/mychattingapp/ChatActivity.kt
+++ app/src/main/java/com/example/mychattingapp/ChatActivity.kt
@@ -1,30 +1,82 @@
 package com.example.mychattingapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class ChatActivity : AppCompatActivity() {
 
     private lateinit var chatReclyclerView: RecyclerView
-    private lateinit var messagebox: EditText
+    private lateinit var messageBox: EditText
     private lateinit var btnSend: ImageView
-//    private lateinit var : ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
+    private lateinit var mDbRef: DatabaseReference
+
+    var senderRoom: String? = null
+    var receiverRoom: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
+
+        val senderUid = FirebaseAuth.getInstance().currentUser?.uid
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        senderRoom = receiverUid + senderUid
+        receiverRoom = senderUid + receiverUid
 
         supportActionBar?.title = name
 
         chatReclyclerView = findViewById(R.id.chat_recyclerview)
-        messagebox = findViewById(R.id.message_box)
+        messageBox = findViewById(R.id.message_box)
         btnSend = findViewById(R.id.btn_send)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+
+        chatReclyclerView.layoutManager = LinearLayoutManager(this)
+        chatReclyclerView.adapter = messageAdapter
+
+        //logic for adding data to recyclerView
+        mDbRef.child("chats").child(senderRoom!!).child("messages")
+            .addValueEventListener(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    messageList.clear()
+
+                    for (postSnapshot in snapshot.children) {
+                        val message = postSnapshot.getValue(Message::class.java)
+                        messageList.add(message!!)
+                    }
+                    messageAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+
+            })
+
+
+        // add message to database
+        btnSend.setOnClickListener {
+
+            val message = messageBox.text.toString()
+            val messageObject = Message(message, senderUid)
+
+            mDbRef.child("chats").child(senderRoom!!).child("messages").push()
+                .setValue(messageObject).addOnSuccessListener {
+                    mDbRef.child("chats").child(receiverRoom!!).child("messages").push()
+                        .setValue(messageObject)
+                }
+            messageBox.setText("")
+        }
     }
 }
